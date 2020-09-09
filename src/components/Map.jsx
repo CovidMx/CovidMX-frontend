4203045cf98c7e745d848cee100ede457def649c
Eduardo Diaz <lalo.diaz1896@gmail.com>
2020-09-08 19:32:44 -0500

    Mapa 1

---
+++ src/components/Map.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Map = () => {
+  return (
+    <div className='Map'>
+      Aquí va el mapa
+      
+    </div>
+  );
+};
+
+export default Map;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 
 import Header from '../components/Header';
+import Map from '../components/Map';
 
 import '../assets/styles/App.scss';
 
 const Home = () => {
   return (
-    <Header />
+    <>
+      <Header />
+      <Map />
+    </>
   );
 };
 
