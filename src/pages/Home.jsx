037bbcf3e2339e26eb24023483e6426e58960967
Eduardo Diaz <lalo.diaz1896@gmail.com>
2020-09-08 21:39:37 -0500

    Test de Mapa 1

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import Header from '../components/Header';
-import Map from '../components/Map';
+import TestMap from '../components/TestMap';
 
 import '../assets/styles/App.scss';
 
@@ -9,7 +9,7 @@ const Home = () => {
   return (
     <>
       <Header />
-      <Map />
+      <TestMap />
     </>
   );
 };
